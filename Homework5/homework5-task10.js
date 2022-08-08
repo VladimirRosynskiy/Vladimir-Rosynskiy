e683e8b79f8e7ccf2de61ec258b864f4b0ff9306
VladimirRosynskiy <vovarolan1@gmail.com>
2022-08-08 18:44:49 +0300

    hw5 dine

---
+++ Homework5/homework5-task10.js
@@ -0,0 +1,22 @@
+// 10) Сделайте функцию inArray, которая определяет, есть в массиве элемент с заданным текстом или нет.
+// Функция первым параметром должна принимать текст элемента, а вторым - массив,
+// в котором делается поиск. Функция должна возвращать true или false.
+
+// let textOne = "Жижа";
+// let arrOne = ["Вова", "Жека", "Катя"]
+// function inArray(text, arr) {
+//     for (let i = 0; i < arr.length; i++) {
+//         if (arr[i] === text) {
+//             return true
+//         }
+//     }
+//     return false
+// }
+// console.log(inArray(textOne, arrOne));
+
+// let textOne = "Жека";
+// let arrOne = ["Вова", "Жека", "Катя"]
+// function inArray(text, arr) {
+//     return arr.indexOf(text) > -1;    ------ почитать
+// }
+// console.log(inArray(textOne, arrOne));
---
+++ Homework5/homework5-task2.js
@@ -0,0 +1,15 @@
+
+// 2) Сделайте функцию isNumberInRange, которая параметром принимает число и проверяет,
+// что оно больше нуля и меньше 10. Если это так - пусть функция возвращает true, если не так - false.
+
+// function isNumberInRange(a) {
+//     if (a > 0 && a < 10) {
+//         return true
+//     } else {
+//         return false
+//     }
+// }
+// console.log(isNumberInRange(1));
+
+
+
